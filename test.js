8da9e74185c7298261458d38cd755d40a42e40f3
luohao <1223048059@qq.com>
2022-07-17 19:28:54 +0800

    new folder Promise

--- test.js
+++ test.js
@@ -1,17 +1,15 @@
-let names = ['iliakan', 'remy', 'jeresig'];
+let names = ["iliakan", "remy", "jeresig"];
 
-let requests = names.map(name => fetch(`https://api.github.com/users/${name}`));
+let requests = names.map((name) =>
+  fetch(`https://api.github.com/users/${name}`)
+);
 
 Promise.all(requests)
-  .then(responses => {
-    // 所有响应都被成功 resolved
-    for(let response of responses) {
-      alert(`${response.url}: ${response.status}`); // 对应每个 url 都显示 200
+  .then((responses) => {
+    for (let response of responses) {
+      console.log(`${response.url}: ${response.status}`);
     }
-
     return responses;
   })
-  // 将响应数组映射（map）到 response.json() 数组中以读取它们的内容
-  .then(responses => Promise.all(responses.map(r => r.json())))
-  // 所有 JSON 结果都被解析："users" 是它们的数组
-  .then(users => users.forEach(user => alert(user.name)));
+  .then((responses) => Promise.all(responses.map((r) => r.json())))
+  .then((users) => users.forEach((user) => console.log(user.name)));
